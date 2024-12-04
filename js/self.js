2f60d28961a8219c9ad05cec2515f74e4886ac39
tunahantarhan <tunahantarhan99@gmail.com>
2024-12-04 23:33:22 +0300

    Feature Commit

--- js/self.js
+++ js/self.js
@@ -1,3 +1,11 @@
 function abonelikTalep() {
     alert("E-postanız listemize eklendi. Artık yeniliklerden en hızlı şekilde haberdar olacaksınız! E-postanızı kontrol etmeyi unutmayın! ");
 }
+
+function defaultColumn(){
+    document.getElementById("duyuru-container-ann").style.flexDirection = "column";
+}
+
+function revertColumn(){
+    document.getElementById("duyuru-container-ann").style.flexDirection = "column-reverse";
+}
